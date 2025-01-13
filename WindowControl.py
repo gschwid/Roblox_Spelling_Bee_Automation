1330afd57e381f2b9910a5c41404b4b3c065ad88
gschwid <gschwid@gmail.com>
2025-01-13 03:09:58 -0600

    Figuring out math to have this work between aspect ratios

--- WindowControl.py
+++ WindowControl.py
@@ -1,6 +1,20 @@
 from subprocess import Popen
 from pywinauto import Application
+from PIL import Image
+import pyautogui
+import ctypes
 
-app = Application()
+#ctypes.windll.user32.SetProcessDPIAware()
+
+app = Application(backend="uia")
 app.connect(title="Roblox")
-print(app.top_window().wrapper_object().rectangle())
+dlg_spec = app.window(title='Roblox')
+rectangle = dlg_spec.wrapper_object().rectangle()
+left, top, right, bottom = rectangle.left, rectangle.top, rectangle.right, rectangle.bottom
+width, height = rectangle.width(), rectangle.height()
+center = rectangle.mid_point()
+centerWidth, centerHeight = center.x, center.y
+print(f'left: {rectangle.left}, top: {rectangle.top}, right: {rectangle.right}, bottom: {rectangle.bottom}')
+print(f"width: {rectangle.width()}, height: {rectangle.height()}")
+
+pyautogui.screenshot(imageFilename="test.png", region=(left +  int((850 / 1920) * width), top + int((600 / 1080) * height), 250, 250))
