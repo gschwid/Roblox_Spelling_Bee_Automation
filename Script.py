3b3c5aa808fca538d3093ad5fe8df5eb161cad25
Grant Schwidder <gschwid@gmail.com>
2025-01-25 21:37:43 -0600

    Have GUI stop and start script.

--- Script.py
+++ Script.py
@@ -12,8 +12,10 @@ class Script:
 
     def startScript(self):
         print("hi")
+        imagesSaved = 0
         self._running = True
         while self._running:
+            print("waaaaaa")
             # Wait for a Roblox window to open
             while not self._visualDetector.checkIfRobloxIsOpen():
                 time.sleep(1)
@@ -21,25 +23,24 @@ class Script:
             
             print("Starting script... \nLooking for roblox charatcer")
             foundMatches = 0
-            while True:
-                match = self._visualDetector.checkIfTurn(imagesSaved)
-                if match:
-                    foundMatches +=1
-                else:
+            match = self._visualDetector.checkIfTurn(imagesSaved)
+            if match:
+                foundMatches +=1
+            else:
+                foundMatches = 0
+            if foundMatches >= 3:
+                found = True
+                validDetection = self._audioHandler.createAudioFile(imagesSaved, 'word.wav', self._visualDetector.waitForRepeatButton) 
+                if validDetection: # This makes sure time out did not happen
+                    sentence = self._audioHandler.transcribeAudio('word.wav')
+                    word = self._audioHandler.getWordFromSentence(sentence)
+                    self._typingHandler.writeWord(word,0.1)
                     foundMatches = 0
-                if foundMatches >= 3:
-                    found = True
-                    validDetection = self._audioHandler.createAudioFile(imagesSaved, 'word.wav', visualDetector.waitForRepeatButton) 
-                    if validDetection: # This makes sure time out did not happen
-                        sentence = self._audioHandler.transcribeAudio('word.wav')
-                        word = self._audioHandler.getWordFromSentence(sentence)
-                        self._typingHandler.writeWord(word,0.1)
-                        foundMatches = 0
-                        imagesSaved += 1
-                        self._visualDetector.waitForRepeatButton(3, 7)
-                        self._visualDetector.handleDeath()
-                    else:
-                        print ("Looks like the button was not found before timed out, must have been a false character detection.")
+                    imagesSaved += 1
+                    self._visualDetector.waitForRepeatButton(3, 7)
+                    self._visualDetector.handleDeath()
+                else:
+                    print ("Looks like the button was not found before timed out, must have been a false character detection.")
 
     def stopScript(self):
         print("bye")
--- gui.py
+++ gui.py
@@ -1,8 +1,20 @@
 from tkinter import *
 from tkinter import ttk
 from Script import Script
+import threading
 
 script = Script()
+thread = None
+
+def threadStart():
+    global thread
+    thread = threading.Thread(target=script.startScript)
+    thread.start()
+
+def killThread():
+    global thread
+    script.stopScript()
+    thread.join()
 
 root = Tk()
 root.title("Roblox Script")
@@ -11,8 +23,8 @@ root.geometry("500x500")
 mainframe = ttk.Frame(root, padding="3 3 3 3")
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 
-start_button = Button(mainframe, text="Start Script", command=script.startScript)
-stop_button = Button(mainframe, text="Stop Script", command=script.stopScript)
+start_button = Button(mainframe, text="Start Script", command=threadStart)
+stop_button = Button(mainframe, text="Stop Script", command=killThread)
 
 start_button.grid(column=0, row=0, sticky="wens", padx=5, pady=5)
 stop_button.grid(column=0, row=1, sticky="wens", padx=5, pady=5)
